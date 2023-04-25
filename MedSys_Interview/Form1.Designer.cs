f793e06412a014d246670cdac8dc17bff2ba0a78
Ben Adams <adamsb32578@gmail.com>
2023-04-25 19:59:09 -0400

    can now add new strings to list

--- MedSys_Interview/Form1.Designer.cs
+++ MedSys_Interview/Form1.Designer.cs
@@ -28,21 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.itemListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Location = new System.Drawing.Point(30, 33);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(661, 20);
+            this.itemTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(697, 31);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // itemListBox
+            // 
+            this.itemListBox.FormattingEnabled = true;
+            this.itemListBox.Location = new System.Drawing.Point(30, 73);
+            this.itemListBox.Name = "itemListBox";
+            this.itemListBox.Size = new System.Drawing.Size(742, 355);
+            this.itemListBox.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.itemListBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.itemTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox itemListBox;
     }
 }
 
--- MedSys_Interview/Form1.cs
+++ MedSys_Interview/Form1.cs
@@ -12,14 +12,20 @@ namespace MedSys_Interview
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
 
+        }
+
+        private void addButton_Click(object sender, EventArgs e) {
+            itemListBox.Items.Add(itemTextBox.Text);
+        }
+
+        private void itemListBox_SelectedIndexChanged(object sender, EventArgs e) {
+            
         }
     }
 }
